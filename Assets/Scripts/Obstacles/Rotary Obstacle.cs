b81f4957ac49fe750dec4d3be3691f29ffa2d725
Joaco <lopezjoaquin488@gmail.com>
2024-05-20 16:25:06 -0300

    Scripts bases

---
+++ Assets/Scripts/Obstacles/Rotary Obstacle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotaryObstacle : MonoBehaviour
+{
+    [SerializeField] int speed;
+
+    private void FixedUpdate()
+    {
+        Spin();
+    }
+
+    private void Spin()
+    {
+        Vector3 rote = new Vector3(0, 1, 0);
+
+        this.transform.Rotate(rote * speed * Time.deltaTime);
+    }
+}
