108b382cb4ee61eeae3c1af6831b41c959fe1aeb
Joaco <lopezjoaquin488@gmail.com>
2024-07-02 20:02:37 -0300

    Building snake

--- Assets/Scripts/Enemies/Snake/Snake.cs
+++ Assets/Scripts/Enemies/Snake/Snake.cs
@@ -17,13 +17,57 @@ public class Snake : Entity
 
     void Start()
     {
-        
+        Player = FindObjectOfType<Player>().transform;
     }
 
     
     void Update()
     {
-        
+        bool playerInRange = Vector3.Distance(transform.position, Player.position) < visionRange;
+        _counter += Time.deltaTime;
+
+        if (playerInRange == true)
+        {
+            //print("Player in range");
+            Vector3 directionToPlayer = (Player.position - transform.position).normalized;
+            directionToPlayer.y = 0;
+
+            Debug.DrawRay(transform.position, directionToPlayer * visionRange, Color.red);
+
+            if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, visionRange, detectableLayers))
+            {
+
+                if (hit.transform.CompareTag("Player"))
+                {
+                    //print("Te veo");
+                    LookPlayer();
+                }
+                else
+                {
+                    //print("No te veo");
+                    RotateSnake();
+                }
+
+            }
+        }
+        else
+        {
+            //print("Player out of range");
+            RotateSnake();
+            /*Vector3 directionToPosition = (positions[index] - transform.position).normalized;
+
+
+            transform.position += directionToPosition * UpDownSpeed * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, positions[index]) < 0.1f)
+            {
+                index++;
+            }
+            if (index >= positions.Length)
+            {
+                index = 0;
+            }*/
+        }
     }
 
     private void MoveUpDown()
@@ -56,5 +100,16 @@ public class Snake : Entity
             Instantiate(VenomBallPrefab, shootingPoint.position, shootingPoint.rotation);
             //print("Te disparo");
         }
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, actionRange);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, visionRange);
+
+        
     }
 }
