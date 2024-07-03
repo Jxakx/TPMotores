87153b3524827d18a9fc79a44e61ce01a465d633
Augus <145805991+au6us@users.noreply.github.com>
2024-07-03 04:14:19 -0300

    Embestida algo meh

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,6 +12,16 @@ public class Player : MonoBehaviour
     private MoveController _moveController;
     [SerializeField] public bool isGrounded;
 
+    public int speedAssaultGoat;
+
+    private bool isCharging = false;
+    private float chargingSpeed = 10.0f; // Velocidad máxima que se puede cargar
+    private float chargeTime = 2.0f; // Tiempo máximo de carga en segundos
+    private float currentChargeTime = 0.0f; // Tiempo actual de carga
+    private float chargeVelocity = 0.0f; // Velocidad actual de la carga
+    private float shootForceMultiplier = 50.0f; // Multiplicador de la fuerza de disparo
+
+
     private void Awake()
     {
         _rB = GetComponent<Rigidbody>();
@@ -24,14 +34,18 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        Walk();
+        if (!isCharging)
+        {
+            Walk();
+        }
         Jump();
+        assaultGoat();
     }
 
     private void Walk()
     {
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        _moveController.Move(direction);
+        _moveController.Move(direction * _speed);
     }
 
     private void Jump()
@@ -73,5 +87,45 @@ public class Player : MonoBehaviour
     private void Dead()
     {
         Destroy(GetComponent<Player>(), 1);
+    }
+
+    public void assaultGoat()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            isCharging = true;
+            // Detener el movimiento del jugador al cargar la embestida
+            _moveController.Move(Vector3.zero);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (currentChargeTime < chargeTime)
+            {
+                currentChargeTime += Time.deltaTime;
+
+            }
+            else
+            {
+                currentChargeTime = chargeTime; // No permitir que el tiempo de carga exceda chargeTime
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            if (isCharging)
+            {
+                // Calcular la fuerza de disparo basada en el tiempo de carga
+                float shootForce = currentChargeTime / chargeTime * chargingSpeed * shootForceMultiplier;
+
+                // Aplicar fuerza hacia adelante al Rigidbody para disparar al jugador
+                _rB.AddForce(transform.forward * shootForce, ForceMode.Impulse);
+
+                // Reiniciar variables de carga
+                isCharging = false;
+                currentChargeTime = 0.0f;
+            }
+        }
+
+
     }
 }
