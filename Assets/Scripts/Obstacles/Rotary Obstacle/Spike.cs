d88aa83b13798a541013ad0e54bdc6b7c0c98492
Joaco <lopezjoaquin488@gmail.com>
2024-07-04 01:01:34 -0300

    spikes

--- Assets/Scripts/Obstacles/Rotary Obstacle/Spike.cs
+++ Assets/Scripts/Obstacles/Rotary Obstacle/Spike.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
-    
-    
+    private int damage = 1;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.ReciveDamage(damage);
+        }
+    }
 }
